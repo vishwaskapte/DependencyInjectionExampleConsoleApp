a5663c14cceb7f0579baa2a6753f3565482d412e
vishw <vishw@VISHWAS-DESKTOP>
2022-11-23 16:08:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace DependencyInjectionExampleConsoleApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmployeeBL employeeBL = new EmployeeBL(new EmployeeDAL());
+        List<Employee> ListEmployee = employeeBL.GetAllEmployees();
+        foreach (Employee emp in ListEmployee)
+        {
+            Console.WriteLine("ID = {0}, Name = {1}, Department = {2}", emp.ID, emp.Name, emp.Department);
+        }
+        Console.ReadKey();
+    }
+
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeeBL
+    {
+        public IEmployeeDAL employeeDAL;
+        public EmployeeBL(IEmployeeDAL employeeDAL)
+        {
+            this.employeeDAL = employeeDAL;
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+
+    public interface IEmployeeDAL
+    {
+        List<Employee> SelectAllEmployees();
+    }
+    public class EmployeeDAL : IEmployeeDAL
+    {
+        public List<Employee> SelectAllEmployees()
+        {
+            List<Employee> ListEmployees = new List<Employee>();
+            //Get the Employees from the Database
+            //for now we are hard coded the employees
+            ListEmployees.Add(new Employee() { ID = 1, Name = "Pranaya", Department = "IT" });
+            ListEmployees.Add(new Employee() { ID = 2, Name = "Kumar", Department = "HR" });
+            ListEmployees.Add(new Employee() { ID = 3, Name = "Rout", Department = "Payroll" });
+            return ListEmployees;
+        }
+    }
+}
